6deba206e64ccc69ae9667650461a3c48be19426
EmpireMU <empiremux@gmail.com>
2025-07-15 13:45:20 +0100

    Updated settings for HTTPS.

--- server/conf/production_settings.py
+++ server/conf/production_settings.py
@@ -92,7 +92,7 @@ WEBSOCKET_CLIENT_URL = "wss://empiremush.org/"
 
 # Django HTTPS settings for reverse proxy
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ['https://empiremush.org']
 USE_TLS = True
