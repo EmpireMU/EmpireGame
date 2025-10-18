7bfc97496d208d43c0a5e9e562dc9d9756a6d301
EmpireMU <empiremux@gmail.com>
2025-10-18 19:41:03 +0100

    Fix to worldinfo admin.

--- web/worldinfo/admin.py
+++ web/worldinfo/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import WorldInfoPage, News
+from .models import WorldInfoPage, MapLocation
 
 
 @admin.register(WorldInfoPage)
 class WorldInfoPageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'subcategory', 'is_public', 'updated_at']
-    list_filter = ['category', 'subcategory', 'is_public']
+    list_display = ['title', 'category', 'updated_at']
+    list_filter = ['category']
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     
@@ -14,27 +14,27 @@ class WorldInfoPageAdmin(admin.ModelAdmin):
             'fields': ('title', 'slug', 'content', 'emblem_image')
         }),
         ('Organization', {
-            'fields': ('category', 'subcategory')
-        }),
-        ('Visibility', {
-            'fields': ('is_public',)
+            'fields': ('category',)
         }),
     )
 
 
-@admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'posted_date', 'is_active', 'order', 'updated_at']
-    list_filter = ['category', 'is_active']
-    search_fields = ['title', 'content']
-    list_editable = ['is_active', 'order']
+@admin.register(MapLocation)
+class MapLocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'map_name', 'location_type', 'x_coord', 'y_coord', 'updated_at']
+    list_filter = ['map_name', 'location_type']
+    search_fields = ['name', 'description']
+    list_editable = ['location_type']
     
     fieldsets = (
-        ('Content', {
-            'fields': ('title', 'content', 'posted_date')
+        ('Location Info', {
+            'fields': ('map_name', 'name', 'description', 'location_type')
         }),
-        ('Display', {
-            'fields': ('category', 'is_active', 'order')
+        ('Coordinates', {
+            'fields': ('x_coord', 'y_coord')
+        }),
+        ('Link', {
+            'fields': ('link_url',)
         }),
     )
 
